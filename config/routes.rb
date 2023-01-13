65126da2b026918a31580d436ecac715c8c74096
sushisyake <117505663+sushisyake@users.noreply.github.com>
2023-01-13 09:37:27 +0000

    add_device

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+# 顧客用
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   #get 'test/index'
   root 'test#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
