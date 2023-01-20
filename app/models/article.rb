bf9548af422e0ce0d95f43a4a3ab3bd17674f346
EC2 Default User <ec2-user@ip-172-31-43-248.ap-northeast-1.compute.internal>
2023-01-20 16:36:02 +0000

    change_user_edit

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,6 @@ class Article < ApplicationRecord
   has_many :article_comments, dependent: :destroy
 
   validates :question, presence:true
-  validates :answer, presence:true
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
