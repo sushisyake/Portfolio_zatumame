a1506865a43c08bcd3a228fac7b8a5b57fedb80d
EC2 Default User <ec2-user@ip-172-31-43-248.ap-northeast-1.compute.internal>
2023-01-29 12:10:47 +0000

    fix_tag_search

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,10 +1,23 @@
 class Public::GenresController < ApplicationController
-  def index
-    @genres = Genre.all
-  end
 
   def show
     @genres = Genre.all
     @genre = Genre.find(params[:id])
+    #タグ検索のための処理
+    if params[:tag_ids].present?
+      tag_ids = params[:tag_ids].select{|key, value| value == "1"}.keys
+    end
+    if tag_ids.present? && tag_ids[0].present?
+      @articles = Tag.find(tag_ids[0]).articles
+      tag_ids.delete_at(0)
+      tag_ids.each do |tag_id|
+        @articles = @articles & Tag.find(tag_id).articles
+      end
+      @articles_count = @articles.count
+      @articles = Kaminari.paginate_array(@articles).page(params[:page])
+    else
+      @articles_count = Article.all.count
+      @articles = Article.all.page(params[:page])
+    end
   end
-end
+  end
