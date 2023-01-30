52c931f432910d26f3b55b86804173ff1db846a6
EC2 Default User <ec2-user@ip-172-31-43-248.ap-northeast-1.compute.internal>
2023-01-30 17:39:58 +0000

    三項演算を実装

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,14 +1,8 @@
 class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
-
   #ユーザーと投稿の検索処理 if文でUserとArticleを区別できる
   def search
     @range = params[:range]
-    if @range == "User"
-      @users = User.looks(params[:word])
-    else
-      @articles = Article.looks(params[:word])
-    end
+    @range == "User" ? @users = User.looks(params[:word]) : @articles = Article.looks(params[:word])
   end
 end
-
